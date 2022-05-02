b5413e898ad217d9981078c34bbd9787d19d3338
Yuki Yuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2022-05-02 11:55:09 +0900

    feat add_task test
    動かない

--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,42 @@ pub mod db_controller {
     // Task型
     pub struct Task {
         #[serde(with = "ts_seconds")]
-        scheduled_date: DateTime<Utc>,
+        pub scheduled_date: DateTime<Utc>,
         #[serde(with = "ts_seconds")]
-        register_date: DateTime<Utc>,
-        title: String,
-        memo: String,
-        finish: bool,
+        pub register_date: DateTime<Utc>,
+        pub title: String,
+        pub memo: String,
+        pub finish: bool,
     }
 
     //新規タスクの追加
-    pub fn add_task(data: Task) -> Result<(), mongodb::error::Error> {
+    pub fn add_task(new_task: Task) -> Result<(), mongodb::error::Error> {
         let client = Client::with_uri_str("mongodb://localhost27017")?;
         let database = client.database("taskdb");
         let collection = database.collection::<Task>("task");
 
-        let docs = vec![data];
+        let docs = vec![new_task];
         collection.insert_many(docs, None)?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::db_controller::{add_task, Task};
+    use chrono::prelude::Utc;
+
+    #[test]
+    fn add_new_task() {
+        let new_task = Task {
+            scheduled_date: Utc::now(),
+            register_date: Utc::now(),
+            title: String::from("LLP"),
+            memo: String::from("Life Love Peace"),
+            finish: false,
+        };
+
+        add_task(new_task).expect("Failed to add new Task");
+        println!("add new Task");
     }
 }
